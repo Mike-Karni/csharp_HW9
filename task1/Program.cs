e525b5de740a633490bc84a1f8b0091765785994
mike_karni <3923499@mail.ru>
2022-05-20 14:28:48 +0300

    Task1

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+N = 5 -> "5, 4, 3, 2, 1"
+
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+int numberStart = Input("Введите значение M: ");
+int numberEnd = 1;
+
+Console.WriteLine(OutputFunction(numberStart, numberEnd));
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int OutputFunction(int numStart, int numEnd)
+{
+    if (numEnd == numStart)
+    {
+        return numEnd;
+    }
+    Console.Write(OutputFunction(numStart, numEnd + 1) + ", ");
+    return numEnd;
+}

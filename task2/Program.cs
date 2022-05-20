087d11cb188fff533c300e1c462a32356dba4d13
mike_karni <3923499@mail.ru>
2022-05-20 14:55:05 +0300

    Task2Done

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+
+M = 4; N = 8. -> 30 */
+
+
+
+
+int numStart = Input("Введите значение N ");
+int numEnd = Input("Введите значение M ");
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int result = RecurSum(numStart,numEnd);
+Console.WriteLine(result + " ");
+
+int RecurSum(int n,int m)
+{
+
+    if (n <= m)
+
+        return n;
+
+    return n + RecurSum(n - 1,m);
+
+} 
